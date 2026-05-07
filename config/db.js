726b2f735d933fa5a8831d55ffc65a3893f8c428
Oluwamayowa <ademayo234@gmail.com>
2026-05-06 23:05:25 -0700

    updated connectDB function

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 async function connectDB() {
+    console.log("connectDB called")
     try {
         await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected")
     } catch (err) {
         console.log("Error detected:", err)
     }
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const dotenv = require("dotenv").config()
 const port = process.env.PORT || 8000
 const eventRoutes = require('./routes/eventRoutes')
 const userRoutes = require('./routes/userRoutes')
+const { connectDB } = require("./config/db")
 
 const app = express()
 
 app.use('/events', eventRoutes)
 app.use('/user', userRoutes)
 
-app.listen(port, () => {console.log(`Server started on port: ${port}`)})
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+    connectDB();
+});
+
