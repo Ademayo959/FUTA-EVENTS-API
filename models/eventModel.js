05c20892eb71f9b96870450f2e109b9b99abcdff
Oluwamayowa <ademayo234@gmail.com>
2026-05-07 16:35:54 -0700

    added the mongoose schemas

---
+++ models/eventModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const eventSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: String, required: true },
+    location: { type: String, required: true },
+    host: { type: String },
+    phoneNumber: { type: String, required: true },
+    guests: { type: Array },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    registrationLink: { type: String, required: true }
+})
+
+const event = mongoose.model('event', eventSchema);
+
+module.exports = event
