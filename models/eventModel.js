02f1a716d297429ca1fee5e0209f850b5e77ad26
Oluwamayowa <ademayo234@gmail.com>
2026-06-30 16:20:17 -0700

    added cors policy to allow the frontend and localhost

--- models/eventModel.js
+++ models/eventModel.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose")
 const eventSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    date: { type: String, required: true },
+    date: { type: Date, required: true },
+    category: { type: String },
     location: { type: String, required: true },
     host: { type: String },
     phoneNumber: { type: String, required: true },
     guests: { type: Array },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    registrationLink: { type: String, required: true }
+    registrationLink: { type: String }
 })
 
 const event = mongoose.model('event', eventSchema);
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT || 8000
+const cors = require('cors')
 const { rateLimit } = require('express-rate-limit')
 const eventRoutes = require('./routes/eventRoutes')
 const userRoutes = require('./routes/userRoutes')
 const { connectDB } = require("./config/db")
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://futaevents.vercel.app'
+]
 
 const app = express()
 
@@ -16,6 +22,18 @@ const limiter = rateLimit({
 
 app.use(limiter)
 
+const corsOption = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1 ) {
+      const msg = 'The CORS policy for this site does not allow ccess from the specified origin';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}
+
+app.use(cors(corsOption))
 app.use(express.json())
 app.use('/events', eventRoutes)
 app.use('/user', userRoutes)
